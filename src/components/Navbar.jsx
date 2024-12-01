0ea07c143e01f560ffa7bb99c7670b5d385d176d
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-12-01 20:49:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
    <nav className="">
       <ul className="flex justify-around items-center bg-lime-950 text-white text-xl sm:text-2xl p-2">
-         <li>&lt; Chat-Bot /&gt;</li>
+         <li>&lt;Chat-Bot/&gt;</li>
          
          <a href="https://github.com/debojyoti-tantra" className='text-white bg-green-700 rounded-full flex my-auto justify-between items-center ring-white ring-1'>
             <img className='invert w-8 sm:w-9 p-1' src="/github.svg" alt="github logo" />
